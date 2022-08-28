df0f7cca4d1a56a3182213fa2711df899bb51667
Eric Kim <erick@aitmed.com>
2022-08-28 04:03:39 -0700

    preliminary implementation of simps command finished

--- cogs/reactions.py
+++ cogs/reactions.py
@@ -32,7 +32,39 @@ class Reactions(commands.Cog):
         self.connection.commit()
     
     @app_commands.command(name="simps", description='Simp Leaderboard')
-    async def simps(self, interaction: discord.Interaction, user: str) -> None:
+    async def simps(self, interaction: discord.Interaction, user: discord.User) -> None:
+        self.cursor.execute(f'''SELECT count(name) FROM sqlite_master WHERE type='table' AND name = '{user.id}' ''')
+
+        # Build Embed Common Fields
+        embed = discord.Embed(color=0xf1d3ed)
+        embed.set_thumbnail(url=user.avatar.url)
+
+        userName = user.name
+        if userName[-1] == 's':
+            embed.add_field(name=f'{user.name}\' simps', value='\u200b', inline=False)
+        else:
+            embed.add_field(name=f'{user.name}\'s simps', value='\u200b', inline=False)    
+        
+        # Build Simps String
+        if self.cursor.fetchone()[0] == 1:
+            self.cursor.execute(f'SELECT * FROM \'{str(user.id)}\' ORDER BY count DESC')
+            simpList = self.cursor.fetchall()
+            print(simpList)
+            result = f''
+            for i in range(min(5, len(simpList))):
+                currentUser = self.bot.get_user(int(simpList[i][0]))
+                if i > 0:
+                    result += f'\n{i+1}) {currentUser.mention} \n\n'
+                else:
+                    result += f'**{i+1}) {currentUser.mention}** \n\n'
+                    embed.set_image(url=currentUser.avatar.url)
+
+            embed.add_field(name='\u200b', value=result, inline=False)
+            await interaction.response.send_message(embed=embed)
+        else:
+            embed.add_field(name='\u200b', value=f'{user.mention} has no simps', inline=False)
+            await interaction.response.send_message(embed=embed)
+
         return
 
 
