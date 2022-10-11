f7a3fb45f0a64634aa173b865889163bf78a3cc6
Eric Kim <erick@Yi.local>
2022-10-11 16:49:54 -0700

    select

--- cogs/games.py
+++ cogs/games.py
@@ -24,6 +24,7 @@ from discord import app_commands
 class Questionnaire(discord.ui.Modal, title='Questionnaire Response'):
     name = discord.ui.TextInput(label='Name')
     answer = discord.ui.TextInput(label='Answer', style=discord.TextStyle.paragraph)
+    attack = discord.ui.Select(options=data.attacks)
 
     async def on_submit(self, interaction: discord.Interaction):
         await interaction.response.send_message(f'Thanks for your response, {self.name}!', ephemeral=True)
