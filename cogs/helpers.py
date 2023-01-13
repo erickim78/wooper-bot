22f8671d322641b55cc585507dbff8acbf347f13
Eric Kim <erick@Yi.local>
2023-01-12 17:42:43 -0800

    update embed formatting

--- cogs/helpers.py
+++ cogs/helpers.py
@@ -38,9 +38,11 @@ class Helpers(commands.Cog):
         else:
             print("Errror")
             return
-       
-        embed = discord.Embed(title="Clock", description=f'Timezone: **{timezone}**', color=0xf1d3ed)
-        embed.add_field(name=currentTime.strftime("%I:%M %p"), value='\u200b', inline=False)
+
+        imgURL = "https://static.wikia.nocookie.net/maplestory/images/3/35/Mob_Papulatus_Clock.png/revision/latest?cb=20110901131435"
+        embed = discord.Embed(title="Papulatus Clock", description=f'Timezone: {timezone}', color=0xf1d3ed)
+        embed.set_thumbnail( url = imgURL )
+        embed.add_field(name=f'It is currently {currentTime.strftime("%I:%M %p")} in {timezone}', value='\u200b', inline=False)
         await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
