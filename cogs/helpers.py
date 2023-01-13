d60c53b7101d953f658087c3cce68d38dfb3375a
Eric Kim <erick@Yi.local>
2023-01-12 17:43:08 -0800

    remove branding

--- cogs/helpers.py
+++ cogs/helpers.py
@@ -40,7 +40,7 @@ class Helpers(commands.Cog):
             return
 
         imgURL = "https://static.wikia.nocookie.net/maplestory/images/3/35/Mob_Papulatus_Clock.png/revision/latest?cb=20110901131435"
-        embed = discord.Embed(title="Papulatus Clock", description=f'Timezone: {timezone}', color=0xf1d3ed)
+        embed = discord.Embed(title="Clock", description=f'Timezone: {timezone}', color=0xf1d3ed)
         embed.set_thumbnail( url = imgURL )
         embed.add_field(name=f'It is currently {currentTime.strftime("%I:%M %p")} in {timezone}', value='\u200b', inline=False)
         await interaction.response.send_message(embed=embed)
